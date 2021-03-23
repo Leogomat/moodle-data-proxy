02cdd8157b0b3066c10da9473ee9ba1877d7bd22
Leogomat <leogomat@gmail.com>
2021-03-23 19:53:06 +0100

    reset delays to original values

--- src/main/i5/las2peer/services/moodleDataProxyService/MoodleDataProxyService.java
+++ src/main/i5/las2peer/services/moodleDataProxyService/MoodleDataProxyService.java
@@ -83,8 +83,8 @@ public class MoodleDataProxyService extends RESTService {
 	private static MoodleStatementGenerator statements = null;
 	private static Context context = null;
 
-	private final static int MOODLE_DATA_STREAM_PERIOD = 20; // Every minute
-	private final static int MOODLE_USER_INFO_UPDATE_PERIOD = 15; // Every hour
+	private final static int MOODLE_DATA_STREAM_PERIOD = 60; // Every minute
+	private final static int MOODLE_USER_INFO_UPDATE_PERIOD = 3600; // Every hour
 	private static long lastChecked = 0;
 	private static String email = "";
 
